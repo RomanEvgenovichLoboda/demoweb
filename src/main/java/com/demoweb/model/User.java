4898fbe9400a36c407634082aaca7e5273949975
RomanEvgenovichLoboda <loboda.rom@gmail.com>
2024-06-09 16:05:06 +0300

    add home, test tailwind

--- src/main/java/com/demoweb/model/User.java
+++ src/main/java/com/demoweb/model/User.java
@@ -1,2 +1,4 @@
-package com.demoweb.model;public class User {
+package com.demoweb.model;
+
+public class User {
 }
--- src/main/java/com/demoweb/web/HomeController.java
+++ src/main/java/com/demoweb/web/HomeController.java
@@ -1,2 +1,10 @@
-package com.demoweb.web;public class HomeController {
+package com.demoweb.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {return "home";}
 }
